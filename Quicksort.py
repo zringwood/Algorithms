fd40bba00160fe1baf62ea61d692edd5957caa8f
Zachary Ringwood <zringwood@gmail.com>
2022-12-16 14:32:02 -0500

    Add files via upload
    Quicksort implementation.

---
+++ Quicksort.py
@@ -0,0 +1,29 @@
+import random, time
+
+def quicksort(values):
+    #Exit condition for the recursion
+    if len(values) <= 1:
+        return values
+    pivot = values[len(values)-1]
+    greater, lesser = [],[]
+    #Split the array around the pivot
+    for i in values :
+        if i > pivot :
+            greater.append(i)
+        else :
+            lesser.append(i)
+    #The algorithm will keep selecting the same pivot if it stays in lesser
+    lesser.pop(len(lesser)-1)
+    return quicksort(lesser) +[pivot]+ quicksort(greater)
+
+size = 100
+values = []
+
+for i in range(size) :
+    values.append(random.randint(0,size))
+
+print(values)
+print(quicksort(values))
+
+
+    
